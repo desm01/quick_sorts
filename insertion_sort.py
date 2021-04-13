0aa3c53ddbfa8d036e6bc92c21211cf58b9474a4
smithers <desmond.madden@hotmail.co.uk>
2021-04-13 23:27:39 +0100

    Added Insertion Sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,26 +1,24 @@
-def insertion_sort(array):
-    
-    length_of_array = len(array)
 
-    for i in range(1, length_of_array):
+
+def insertion_sort(array):
+    array_length = len(array)
+
+    for i in range(1, array_length):
+
         currentElement = array[i]
 
-        j = i - 1
-        
-        #   Shift elements that are greater than currentElement up once
-        
-        while (j >= 0 and array[j] > currentElement):
-            
-            array[j+1] = array[j]
+        j = i -1
 
+        while j >= 0 and array[j] >= currentElement:
+            array[j+1] = array[j]
             j = j -1
 
-        array[j + 1] = currentElement 
+        array[j + 1] = currentElement
 
     return array
 
-
 array = [10,12,4,5,2,1,199,21,32,1,90,18]
+
 
 insertion_sort(array)
 
